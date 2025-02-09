1f4d7d39cdb68cc0e308e043a2e0a0513dc51952
MateuszPietkiewicz <mateuszpietkiewicz1@gmail,com>
2025-02-09 10:51:11 +0100

    template_project

---
+++ app/main.py
@@ -0,0 +1,6 @@
+def add(a: int, b: int) -> float | None:
+    return None if a >42 else 42
+
+x: float | None= add(1,2)
+
+result = x +42
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from flashcards-ai-obsidian-pro!")
-
-
-if __name__ == "__main__":
-    main()
